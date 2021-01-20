28d5f7526aa5022d8c362becc467f18d9df76dd7
Samuel Lee <samuelxlee@gmail.com>
2021-01-20 15:56:56 -0800

    configured db-config file, wrote find, findById, and findSteps functions for endpoints

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,33 @@
 // scheme-model
+const db = require("../../data/db-config");
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    // add,
+    // update,
+    // remove
+}
+
+function find() {
+    return db('schemes');
+}
+
+function findById(id) {
+    const schemeObj = db('schemes').where('id', id).first();
+
+    if (!schemeObj) {
+        return Promise.resolve(null);
+    } else {
+        return schemeObj;
+    }
+}
+
+function findSteps(id) {
+    return db('schemes as sc')
+        .join('steps as st', 'sc.id', 'st.scheme_id')
+        .select('sc.id', 'scheme_name', 'step_number', 'instructions')
+        .where('sc.id', id)
+        .orderBy('step_number');
+}
